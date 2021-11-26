ad86e67440659d47128444f45cdaec453a865f3d
Ferdinand Malcher <ferdinand@malcher.media>
2021-11-26 13:33:03 +0100

    Change Detection

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent implements OnInit {
   @Output() rateUp = new EventEmitter<Book>();
@@ -25,6 +26,10 @@ export class BookComponent implements OnInit {
 
   onRateDown() {
     this.rateDown.emit(this.book);
+  }
+
+  log() {
+    console.log('CD', Date.now());
   }
 
 }
