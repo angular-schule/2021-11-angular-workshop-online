429545d35668ddd0b4d16c475ec296442d137c91
Ferdinand Malcher <ferdinand@malcher.media>
2021-11-29 14:34:41 +0100

    Formulare (Anfang)

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'br-book-create',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookCreateComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup;
+
+  constructor() {
+    this.bookForm = new FormGroup({
+      isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13)
+      ]),
+      title: new FormControl('', Validators.required),
+      description: new FormControl(''),
+      price: new FormControl(0, [
+        Validators.min(0),
+        Validators.max(1000),
+      ]),
+      rating: new FormControl(1, [
+        Validators.min(1),
+        Validators.max(5),
+      ]),
+    });
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+/*
+TODO:
+- Validierung
+- Hinweismeldungen
+- Submit-Button
+- Abschicken
+- HTTP
+- bei Erfolg: Redirect zur Detailseite
+*/
