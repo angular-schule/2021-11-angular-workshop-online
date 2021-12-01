57e4785e1a24e99d47469a05e4297556ffb53bdf
Ferdinand Malcher <ferdinand@malcher.media>
2021-12-01 11:54:19 +0100

    Detailseite: RxJS

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { map, mergeMap, switchMap } from 'rxjs';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -18,15 +19,14 @@ export class BookDetailsComponent implements OnInit {
     // console.log(isbn);
 
     // Asynchroner Weg (PUSH)
-    // TODO: Verschachtelte Subscriptions
-    this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn')!;
-      console.log(isbn);
 
-      this.bs.getSingle(isbn).subscribe(book => {
-        this.book = book;
-      });
+    this.route.paramMap.pipe(
+      map(params => params.get('isbn')!),
+      switchMap(isbn => this.bs.getSingle(isbn)),
+    ).subscribe(book => {
+      this.book = book;
     });
+
 
 
     // Aufgabe: Buch laden und anzeigen
