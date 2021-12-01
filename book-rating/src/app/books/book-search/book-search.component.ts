2e06008f62a9f195e38e79be06cce857256f7c61
Ferdinand Malcher <ferdinand@malcher.media>
2021-12-01 12:41:40 +0100

    Typeahead-Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { debounceTime, filter, Observable, switchMap } from 'rxjs';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-search',
@@ -10,11 +13,23 @@ export class BookSearchComponent implements OnInit {
 
   searchControl = new FormControl('');
 
-  constructor() {
-    this.searchControl.valueChanges.subscribe(e => {
-      console.log(e);
-    });
+  books$: Observable<Book[]>;
+
+  constructor(private bs: BookStoreService) {
+    this.books$ = this.searchControl.valueChanges.pipe(
+      debounceTime(1000),
+      filter(term => term.length >= 3 || term.length === 0),
+      switchMap(term => this.bs.search(term))
+    );
   }
+
+  /* Typeahead-Suche
+  - Suchbegriff mindestens 3 Zeichen lang (mit RxJS lösen!)
+  - nicht zu viele Begriffe kurz nacheinander abschicken
+  - Bücher auf dem Server suchen (HTTP)
+  - Ergebnisse anzeigen (ganz einfach)
+  - (AsyncPipe)
+  */
 
   ngOnInit(): void {
   }
